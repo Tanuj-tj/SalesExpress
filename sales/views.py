b69a57679ffd674e7b23f7a14aefcc55416194cf
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-06-24 21:48:14 +0530

    24/06/2021

--- sales/views.py
+++ sales/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from sales.models import Sale
+from sales.models import Position, Sale
 from sales.forms import SalesSearchForm
+import pandas as pd
+from sales.utils import get_customer_from_id , get_salesman_from_id
 
 # Create your views here.
 
 def home_view(request):
+
+    sales_df = None
+    positions_df = None
+    merged_df = None
+    df = None
+
     form = SalesSearchForm(request.POST or None)
     
     if request.method == 'POST':
@@ -13,10 +21,49 @@ def home_view(request):
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
 
-        print(date_from,date_to,chart_type)
+        #print(date_from,date_to,chart_type)
+    
+        sale_qs = Sale.objects.filter(created__date__lte = date_to, created__date__gte = date_from)
+        if(len(sale_qs) > 0):
+            sales_df = pd.DataFrame(sale_qs.values())
+            sales_df['customer_id'] = sales_df['customer_id'].apply(get_customer_from_id)
+            sales_df['salesman_id'] = sales_df['salesman_id'].apply(get_salesman_from_id)
+            sales_df['created'] = sales_df['created'].apply(lambda x:x.strftime('%Y-%m-%m'))
+            sales_df.rename({'customer_id':'customer','salesman_id':'salesman', 'id':'sales_id'},axis=1,inplace=True)
+            position_data = []
+            for sale in sale_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        'position_id' : pos.id,
+                        'product' : pos.product.name,
+                        'quantity':pos.quantity,
+                        'price':pos.price, 
+                        'sales_id':pos.get_sales_id()
+                    }
+
+                    position_data.append(obj)
+
+            positions_df = pd.DataFrame(position_data)
+            merged_df = pd.merge(sales_df, positions_df,on='sales_id')
+            
+            df = merged_df.groupby('transaction_id', as_index=False)['price'].agg('sum')
+
+            sales_df = sales_df.to_html()
+            positions_df = positions_df.to_html()
+            merged_df = merged_df.to_html()
+            df = df.to_html()
+        
+        else:
+            print("No data")
+        
+        
 
     context = { 
         'form':form,
+        'sales_df':sales_df,
+        'positions_df':positions_df,
+        'merged_df':merged_df,
+        'df':df,
     }
 
     return render(request, 'sales/home.html' , context)
