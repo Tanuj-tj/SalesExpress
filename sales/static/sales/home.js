d5917c3cf1e7fc888c2132466680ea414bb46afb
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-06-27 16:52:31 +0530

    27/06/2021

--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -6,6 +6,16 @@ const img = document.getElementById('img')
 
 const modalBody = document.getElementById('modal-body')
 
+const reportForm = document.getElementById('report-form')
+
+const reportName = document.getElementById('id_name')
+
+const reportRemarks = document.getElementById('id_remarks')
+
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
+
+console.log(csrf)
+
 console.log(reportBtn)
 console.log(img)
 
@@ -17,4 +27,13 @@ reportBtn.addEventListener('click', ()=>{
     console.log('clicked')
     img.setAttribute('class', 'w-100')
     modalBody.prepend(img)
-} )
+
+    reportForm.addEventListener('submit', e=>{
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('csrfmiddlewaretoken', csrf)
+        formData.append('name',reportName.value)
+        formData.append('remark',reportRemarks.value)
+        formData.append
+    })
+})
